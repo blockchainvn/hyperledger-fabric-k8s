c1b38db8de86cee92fef21b31332f47003740fde
John Tu <tupt@qsoftvietnam.com>
2018-02-07 09:36:20 +0700

    give some hint about sshpass

--- fnssh.sh
+++ fnssh.sh
@@ -7,6 +7,13 @@
 #
 # export so other script can access
 
+if [[ ! command -v sshpass ]];then
+  echo "Try to install sshpass:"
+  echo "apt-get install sshpass"
+  echo "brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb"
+  exit
+fi
+
 CONFIG=$1
 shift
 QUERY="$@"
