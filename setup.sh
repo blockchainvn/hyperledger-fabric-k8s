18c2e1c7398469bd60e18f1de06ff0d3874b8a4c
John Tu <tupt@qsoftvietnam.com>
2018-03-15 14:00:09 +0700

    updated for mac

--- setup.sh
+++ setup.sh
@@ -63,7 +63,11 @@ EOF
 
   # dashboard
   kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
-  # for MACOSX
+
+  # for MACOSX, if not using docker with kubernetes
+  # curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/kubectl
+  # curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.18.0/minikube-darwin-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+  # minikube start 
   # nohup kubectl proxy --accept-hosts='^.*' --accept-paths='^.*' --address='0.0.0.0' --port=8001 > /dev/null 2>&1 & echo $! > dashboard.pid
 
   # check pods
